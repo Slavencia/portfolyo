0ed710e34466307356ab86047142bec54da078ba
Slavencia <clavakopylov2010@gmail.com>
2024-12-03 20:01:06 +0300

    Add files via upload

---
+++ New_proekt 29.11/main.py
@@ -0,0 +1,31 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    
+    e_mail = request.form.get('email')
+    text = request.form.get('text')
+    print(e_mail, text)
+    button_python = request.form.get('button_python')
+
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+
+    return render_template('index.html', button_python=button_python, button_discord=button_discord, button_db=button_db, button_html=button_html)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
